a852490f1540ba224e6b6622eb4ac8cd36987919
Klaucio <clauciozitha@gmail.com>
2015-08-20 19:17:04 +0200

    Classe mensalidade

---
+++ SistemaEscolar/src/uem/poo/sistema/escola/modelo/Mensalidade.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uem.poo.sistema.escola.modelo;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author claucio
+ */
+@Entity
+public class Mensalidade {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long codigo;
+    @Temporal(TemporalType.DATE)
+    private Date dataVencimento;
+    @Temporal(TemporalType.DATE)
+    private Date dataPagamento;
+    @Column(nullable = false)
+    private double valorApagar;
+    @Column(nullable = false)
+    private double valorPago;
+    
+    
+
+    public Mensalidade(Date dataVencimento, Date dataPagamento, double valorApagar, double valorPago) {
+        
+        this.dataVencimento = dataVencimento;
+        this.dataPagamento = dataPagamento;
+        this.valorApagar = valorApagar;
+        this.valorPago = valorPago;
+    }
+        
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public double getValorApagar() {
+        return valorApagar;
+    }
+
+    public void setValorApagar(double valorApagar) {
+        this.valorApagar = valorApagar;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+    
+}
